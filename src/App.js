7eb0b4f9a86a500c571d125814bc071babc524f7
Leilani Migisi <leilani.migisi@student.moringaschool.com>
2022-07-31 22:49:36 +0300

    Deploy

--- src/App.js
+++ src/App.js
@@ -6,68 +6,75 @@ import MovieListHeading from "./components/MovieListHeading";
 import SearchBox from "./components/SearchBox";
 import AddFavourites from "./components/AddFavourites";
 import RemoveFavourites from "./components/RemoveFavourites";
+// import { Routes } from "react-router-dom";
 
 
 const App = () => {
+
   const [movies, setMovies] = useState([]);
-    const [favourites, setFavourites] = useState([]);
-    const [searchValue, setSearchValue] = useState([]);
-
-
-    useEffect(() => {
-        fetch('http://localhost:3000/movies')
-            .then((res) => res.json())
-            .then((data) => setMovies(data))
-    }, [searchValue]);
+  const [favourites, setFavourites] = useState([])
+  const [searchValue, setSearchValue] = useState([])
+  
+  useEffect(() => {
+    fetch(`localhost:3000/movie/`)
+    .then((res) => res.json())
+    .then((data) => setMovies(data))
+    console.log()
+},[searchValue])
 
 
 const saveToLocalStorage = (items) => {
-        localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
-};
-    
-    
-    const addFavouriteMovie = (movie) => {
-        const newFavouriteList = [...favourites, movie];
-        setFavourites(newFavouriteList);
-        saveToLocalStorage(newFavouriteList);
-    };
+		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+	};
 
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
 
-    const removeFavouriteMovie = (movie) => {
-        const newFavouriteList = favourites.filter(
-            (favourite) => favourite.imdbID !== movie.imdbID
-        );
-        setFavourites(newFavouriteList);
-        saveToLocalStorage(newFavouriteList);
-    };
+	const removeFavouriteMovie = (movie) => {
+		const newFavouriteList = favourites.filter(
+			(favourite) => favourite.imdbID !== movie.imdbID
+		);
 
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
 
-    return (
-        <>
-  <div className='container-fluid movie-app'>
-            <div className='row d-flex align-items-center mt-4 mb-4'>
-                <MovieListHeading heading='MOVIES HUB' />
-                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-            </div>
-            <div className='row'>
-                <MovieList
-                    movies={movies}
-                    handleFavouritesClick={addFavouriteMovie}
-                    favouriteComponent={AddFavourites}
-                />
-            </div>
-            <div className='row d-flex align-items-center mt-4 mb-4'>
-                <MovieListHeading heading='Favourites' />
-            </div>
-            <div className='row'>
-                <MovieList
-                    movies={favourites}
-                    handleFavouritesClick={removeFavouriteMovie}
-                    favouriteComponent={RemoveFavourites}
-                />
-            </div>
-            </div>
-</>    );
+  
+
+ 
+ 
+return (
+  
+	<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4 '>
+				
+				<MovieListHeading heading='Movies Watchlist' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+			<div className="col-md-3"></div>
+				<MovieList
+					movies={movies}
+					handleFavouritesClick={addFavouriteMovie}
+					favouriteComponent={AddFavourites}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+			<div className="col-md-3"></div>
+				<MovieList
+					movies={favourites}
+					handleFavouritesClick={removeFavouriteMovie}
+					favouriteComponent={RemoveFavourites}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default App;
